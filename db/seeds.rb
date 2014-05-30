3ee2c60fc0103fbd1c8cc02954fbd0ffd1caca0f
stolbukhin <maxstbn@gmail.com>
2014-05-30 20:08:50 +0400

    add admin user

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,10 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+5.times do |i|
+  Page.create(title: "Page ##{i}",
+              body:
+                  "This file should contain all the record creation needed to seed the database with its default values. 
+                  The data can then be loaded with the rake db:seed (or created alongside the db with db:setup)."
+  )
+end
