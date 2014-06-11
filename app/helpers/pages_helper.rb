2a60e659e4b199cb9cf957be7f0166b76daf2198
maxstbn <maxstbn@gmail.com>
2014-06-12 01:35:01 +0400

    переводы для модулей

--- app/helpers/pages_helper.rb
+++ app/helpers/pages_helper.rb
@@ -3,8 +3,10 @@ module PagesHelper
   def module_to(modules, position)
     html = ""
     for mod in modules
+      @html_module_language = mod.html_module_languages.where(lang: locale).first
       #html += mod.name == position ? "<h2>#{mod.title}</h2>" : ""
-      html += mod.name == position ? mod.body : ""
+      #html += mod.name == position ? mod.body : ""
+      html += mod.name == position ? @html_module_language.body : ""
     end
     html.html_safe
   end
